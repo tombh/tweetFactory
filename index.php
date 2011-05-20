f1953d2557e19c3bef7d363bbe82ee0593559f99
tombh <tom@tombh.co.uk>
2011-05-20 18:19:54 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+
+  <title>tweetFactory</title>
+
+  <link rel="stylesheet" href="styles.css" type="text/css" media="screen" />  
+
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"></script>
+
+  <script src="/js/main.js" type="text/javascript"></script>
+  <script src="/js/jquery.timeago.js" type="text/javascript"></script>
+
+</head>
+
+<body>
+  <div id="container">
+    <div id="logo">
+      <a href="/">
+        <h1>tweetFactory example</h1>
+      </a>
+    </div>
+    
+    <div id="tweets">
+      &nbsp;
+      <ul>
+        <li class="template" style="display:none">
+          <a class="avatar_link" href=""><img class="profile_pic" src="" width="52" height="52" /></a>
+          <div class="tweet_body">
+          </div>
+          <div class="meta">
+            By <strong><a href="" class="handle">@</a></strong> <abbr class="timeago" title=""></span>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div id="right">
+      <strong>tweetFactory is awesome!</strong>
+      <div id="description">        
+      </div>
+      </small>
+    </div>
+
+  </div>
+</body>
+
+</html>
